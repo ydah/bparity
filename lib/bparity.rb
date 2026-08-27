b1255f9a7d54bb0f583f3a4f4c82495049ce5b0a
ydah <t.yudai92@gmail.com>
2026-08-28 08:01:05 +0900

    Initial commit

---
+++ lib/bparity.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bparity/version"
+
+module Bparity
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/bparity_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bparity do
+  it "has a version number" do
+    expect(Bparity::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
